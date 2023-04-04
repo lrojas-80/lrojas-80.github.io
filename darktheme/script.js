02b6e3689e6e39d1be378d100aae6a0ad3eafc3a
Luis Ariel Rojas <luis.rojas.240980@gmail.com>
2023-04-04 06:09:12 -0300

    main darktheme

---
+++ darktheme/script.js
@@ -0,0 +1,8 @@
+let body =document.querySelector('body');
+let toggle = document.getElementById('toggle');
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
+
+};
+
