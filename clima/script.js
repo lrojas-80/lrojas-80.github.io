1468ff23aabdbeb0037c3687240a3f3fce1c2ba4
Luis Ariel Rojas <luis.rojas.240980@gmail.com>
2023-04-13 16:24:28 -0300

    nuevo proyecto clima

---
+++ clima/script.js
@@ -0,0 +1,56 @@
+/*9f91c1ff388e5100d6bf8312b433427b
+*/
+let weather = {
+    apiKey: "9f91c1ff388e5100d6bf8312b433427b",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No se encuentra la ubicación");
+            throw new Error("Hubo un error.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon/*, description*/ } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "El tiempo en " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      //document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humedad:  " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Velocidad del viento: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("Yacanto, AR");
